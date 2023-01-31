cb3b1dd66ffe678b0c6d09ae96b27911a1c6cf9d
ray <temi@temis-MacBook-Pro.local>
2023-01-31 09:51:56 -0800

    new push

--- funminehan_project/settings.py
+++ funminehan_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vu-y@mi5l7vyilc8xnwr+19hj0=v%()i=n66ru1wu$1kki#t-v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'funminehan_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'metamxtv_fn_db1',
+        'USER': 'metamxtv_fn_user',
+        'PASSWORD': 'fn_user123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -138,7 +142,8 @@ EMAIL_HOST_USER = 'odiagaraymondray@gmail.com'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = '/home/metamxtv/funminehanfoundation.org/static'
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = '/home/metamxtv/funminehanfoundation.org/media'
